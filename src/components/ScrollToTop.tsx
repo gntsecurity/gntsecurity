679d665a889714f5f6679e16ea5a16cff0b607ea
GNT Security LLC <gsmith@gntsecurity.com>
2026-02-27 01:16:07 -0500

    ...

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,25 +1,13 @@
-// src/App.tsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { routes } from "./router/routes";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import ScrollToTop from "./components/ScrollToTop";
+// src/components/ScrollToTop.tsx
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <div className="min-h-screen flex flex-col bg-white text-slate-900">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            {routes.map((route) => (
-              <Route key={route.path} path={route.path} element={route.element} />
-            ))}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
 }
